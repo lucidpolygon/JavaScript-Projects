c8091994d9436c4bd8a27020513daeb84265310f
Thaha Wahid <thahawahid@Thahas-MacBook-Pro.local>
2022-09-04 23:25:39 +0400

    Basic Game Logic Done

---
+++ Wordle/app.js
@@ -0,0 +1,136 @@
+const tileDisplay = document.querySelector(".tile-container");
+const keyboard = document.querySelector(".key-container");
+const messageDisplay = document.querySelector(".message-container");
+
+let currentRow = 0;
+let currentTile = 0;
+let isGameOver = false;
+let wordle = "SUPER";
+
+const keys = [
+  "Q",
+  "W",
+  "E",
+  "R",
+  "T",
+  "Y",
+  "U",
+  "I",
+  "O",
+  "P",
+  "A",
+  "S",
+  "D",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  "ENTER",
+  "Z",
+  "X",
+  "C",
+  "V",
+  "B",
+  "N",
+  "M",
+  "«",
+];
+
+const guessRows = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+
+keys.forEach((key) => {
+  const buttonElement = document.createElement("button");
+  buttonElement.textContent = key;
+  buttonElement.setAttribute("id", key);
+  buttonElement.addEventListener("click", () => handleClick(key));
+  keyboard.append(buttonElement);
+});
+
+guessRows.forEach((guessRow, guessRowIndex) => {
+  const rowElement = document.createElement("div");
+  rowElement.setAttribute("id", "guessRow-" + guessRowIndex);
+  guessRow.forEach((guess, guessIndex) => {
+    const tileElement = document.createElement("div");
+    tileElement.setAttribute(
+      "id",
+      "guessRow-" + guessRowIndex + "-tile-" + guessIndex
+    );
+    tileElement.classList.add("tile");
+    rowElement.append(tileElement);
+  });
+  tileDisplay.append(rowElement);
+});
+
+const handleClick = (key) => {
+  console.log("clicked", key);
+  if (key === "«") {
+    deleteLetter();
+    return;
+  }
+  if (key === "ENTER") {
+    checkRow();
+    return;
+  }
+  addLetter(key);
+};
+
+const addLetter = (letter) => {
+  if (currentTile < 5 && currentRow < 6) {
+    const tile = document.getElementById(
+      "guessRow-" + currentRow + "-tile-" + currentTile
+    );
+    tile.textContent = letter;
+    tile.setAttribute("data", letter);
+    guessRows[currentRow][currentTile] = letter;
+    currentTile++;
+  }
+};
+
+const deleteLetter = () => {
+  if (currentTile > 0) {
+    currentTile--;
+    const tile = document.getElementById(
+      "guessRow-" + currentRow + "-tile-" + currentTile
+    );
+    tile.textContent = "";
+    guessRows[currentRow][currentTile] = "";
+    tile.setAttribute("data", "");
+  }
+};
+
+const checkRow = () => {
+  const guess = guessRows[currentRow].join("");
+  if (currentTile > 4) {
+    if (guess === wordle) {
+      showMessage("Magnificent!");
+      isGameOver = true;
+      return;
+    } else {
+      if (currentRow >= 5) {
+        showMessage("Game Over!");
+        isGameOver = false;
+        return;
+      }
+      if (currentRow < 5) {
+        currentRow++;
+        currentTile = 0;
+      }
+    }
+  }
+};
+
+const showMessage = (message) => {
+  const messageElement = document.createElement("p");
+  messageElement.textContent = message;
+  messageDisplay.append(messageElement);
+  setTimeout(() => messageDisplay.removeChild(messageElement), 2000);
+};
